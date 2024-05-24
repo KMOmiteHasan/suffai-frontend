31c64e52562d1c08df7a1ad9dc479df46a59d614
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2024-05-24 23:50:07 +0600

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import "./global.css";
 import "./main.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
-import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+// import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
         <AppRoutes />
-      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
